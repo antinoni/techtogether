86aea7357fc787fb63fa0fdd523cb25a6286bc56
hannahjdong <>
2020-12-06 11:47:49 -0500

    some board stuff

--- client/src/components/BoardComponent.js
+++ client/src/components/BoardComponent.js
@@ -1,36 +1,53 @@
-import * as React from 'react';
-import { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import * as React from "react";
+import { useState } from "react";
+import "./BoardComponent.css";
+import elderlyPic from "../assets/elderlyboard.png";
+import momPic from "../assets/singlemom.png";
+import tab1 from "../assets/tab1.png";
+import tab2 from "../assets/tab2.png";
+
+import Grid from "@material-ui/core/Grid";
 
 import { db } from "../App";
 
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { boardarray: [] };
+  }
+  componentDidMount() {
+    db.collection("Boards")
+      .get()
+      .then((query) => {
+        console.log(query.docs);
+        const data = query.docs.map((doc) => doc.data());
+        console.log(data);
+        this.setState({ boardarray: data });
+      });
+  }
 
-class Board extends React.Component{
-	constructor(props){
-		super(props);
-		this.state = { boardarray: [] };
-	}
-	componentDidMount(){
-		db.collection('Boards')
-			.get()
-			.then(query => {
-			console.log(query.docs);
-			const data = query.docs.map(doc => doc.data());
-			console.log(data);
-			this.setState({boardarray: data});
-		});
-	}
+  render() {
+    const { boardarray } = this.state;
 
-	render(){
-		const { boardarray } = this.state;
-		return (
-			<div>
-			{ boardarray.map(board => (
-				<div>{board.boardname} </div>
-			))}
-			</div>
-		);
-	}
+    return (
+      <div>
+        <Grid container direction={"row"}>
+          <Grid item xs={12}>
+            <h1>JOIN A GROUP</h1>
+          </Grid>
+          <Grid item xs={6} sm={6}>
+            <img className="tab" src={tab1} alt="elderly picture" />
+          </Grid>
+          <Grid item xs={6} sm={6}>
+            <img className="tab" src={tab2} alt="single mom picture" />
+          </Grid>
+        </Grid>
+        {boardarray.map((board) => (
+          <div>{board.boardname} </div>
+        ))}
+      </div>
+    );
+  }
 }
 
- export default Board
+export default Board;
